08aa3bf030415a88056a0772da88a8451e2900d4
CharmingCheol <toby0806@naver.com>
2023-01-26 23:35:05 +0900

    video-edit.d.ts 추가

---
+++ @types/video-edit.d.ts
@@ -0,0 +1,35 @@
+interface Animator {
+  play(): void;
+
+  pause(): void;
+
+  stop(): void;
+}
+
+interface Downloader {
+  download(): void;
+}
+
+interface TimeController {
+  toSec(): void;
+
+  range(): void;
+}
+
+interface ModuleRegistry {
+  animator: Animator;
+
+  downloader: Downloader;
+
+  timeController: TimeController;
+}
+
+abstract class Module implements OnInit, OnDestroy {
+  constructor(protected moduleRegistry: ModuleRegistry) {}
+
+  abstract init(): void;
+
+  abstract destroy(): void;
+}
+
+interface Engine extends ModuleRegistry, OnInit, OnDestroy {}
