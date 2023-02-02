e179f43c64545022f0f1e367a4221c10fd9c7297
CharmingCheol <toby0806@naver.com>
2023-02-02 22:22:16 +0900

    VideoEditTimeController 메서드 구현

--- src/services/modules/VideoEditTimeController.ts
+++ src/services/modules/VideoEditTimeController.ts
@@ -1,18 +1,46 @@
 class VideoEditTimeController extends VideoEditModule implements TimeController {
+  private _currentTime!: number;
+  get currentTime(): number {
+    return this._currentTime;
+  }
+
+  private _loopRange!: LoopRange;
+  get loopRange(): LoopRange {
+    return { ...this._loopRange };
+  }
+
   public init(): void {
-    throw new Error("Method not implemented.");
+    this._currentTime = 0;
+    this._loopRange = { start: 0, end: this.video.duration };
   }
 
   public destroy(): void {
-    throw new Error("Method not implemented.");
+    this._currentTime = 0;
+    this._loopRange = { start: 0, end: 0 };
   }
 
-  public toSec() {
-    throw new Error("Method not implemented.");
+  public clamp(loopRange: Partial<LoopRange>): LoopRange {
+    this._loopRange = { ...this._loopRange, ...loopRange };
+    if (this._loopRange.start < 0) {
+      this._loopRange.start = 0;
+    }
+    if (this.video.duration < this._loopRange.end) {
+      this._loopRange.end = this.video.duration;
+    }
+    this.moveTo(this._currentTime);
+    return this._loopRange;
   }
 
-  public range() {
-    throw new Error("Method not implemented.");
+  public moveTo(currentTime: number): number {
+    this._currentTime = currentTime;
+    if (this._currentTime < this._loopRange.start) {
+      this._currentTime = this._loopRange.start;
+    }
+    if (this._loopRange.end < this._currentTime) {
+      this._currentTime = this._loopRange.end;
+    }
+    this.video.currentTime = this._currentTime;
+    return this._currentTime;
   }
 }
 
