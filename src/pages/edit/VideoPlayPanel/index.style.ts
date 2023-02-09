4bdc4dedc55092667271d2dca37df4865dff98c3
CharmingCheol <toby0806@naver.com>
2023-02-10 00:07:30 +0900

    video도 화면 크기에 맞게 조절 되도록 변경

--- src/pages/edit/VideoPlayPanel/index.style.ts
+++ src/pages/edit/VideoPlayPanel/index.style.ts
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 export const Panel = styled.main`
-  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
   flex: auto;
+  width: 100%;
+  overflow: hidden;
   background-color: blue;
+`;
+
+export const Video = styled.video`
+  position: absolute;
+  width: 100%;
+  height: 100%;
 `;
