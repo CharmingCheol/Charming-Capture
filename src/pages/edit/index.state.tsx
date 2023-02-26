b1ca8e03d3d43a4652d0de41a8d1d8d26b41f9d4
CharmingCheol <toby0806@naver.com>
2023-02-26 18:06:35 +0900

    EditPageProvider 리팩토링

--- src/pages/edit/index.state.tsx
+++ src/pages/edit/index.state.tsx
@@ -1,16 +1,22 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 
-type ContextApiParams = [typeof initialState, React.Dispatch<React.SetStateAction<typeof initialState>>];
+type ContextApiParams = typeof initialState & {
+  setState: React.Dispatch<React.SetStateAction<typeof initialState>>;
+};
 
 const initialState = { isPlaying: false };
 
+const EditPageContext = createContext<ContextApiParams>({ ...initialState, setState: () => {} });
+
 const EditPageProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, setState] = useState(initialState);
-  const value: ContextApiParams = useMemo(() => [state, setState], [state]);
-
+  const value = useMemo(() => ({ ...state, setState }), [state]);
   return <EditPageContext.Provider value={value}>{children}</EditPageContext.Provider>;
 };
 
-export const EditPageContext = createContext<ContextApiParams>([initialState, () => {}]);
+export const useEditPageContext = () => {
+  const value = useContext(EditPageContext);
+  return value;
+};
 
 export default EditPageProvider;
