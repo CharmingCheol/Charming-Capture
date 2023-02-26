1be0ae5a99c2d4f40b393a0d18c762cabb3ac1fc
CharmingCheol <toby0806@naver.com>
2023-02-26 15:23:17 +0900

    module을 다시 contructor 시점에 생성 되도록 변경

--- src/services/VideoEditEngine.ts
+++ src/services/VideoEditEngine.ts
@@ -1,7 +1,18 @@
 import VideoEditModule from "./modules";
+import VideoEditAnimator from "./modules/VideoEditAnimator";
+import VideoEditDownloader from "./modules/VideoEditDownloader";
+import VideoEditTimeController from "./modules/VideoEditTimeController";
 
 class VideoEditEngine implements OnInit, OnDestroy {
-  constructor(public modulePackage: VideoEditModulePackage) {}
+  public modulePackage!: VideoEditModulePackage;
+
+  constructor() {
+    this.modulePackage = {
+      animator: new VideoEditAnimator(),
+      downloader: new VideoEditDownloader(),
+      timeController: new VideoEditTimeController(),
+    };
+  }
 
   public init(video: HTMLVideoElement): void {
     const modules: VideoEditModule[] = Object.values(this.modulePackage);
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,16 +2,6 @@ import VideoRecord from "./VideoRecord";
 import VideoEditEngine from "./VideoEditEngine";
 import VideoUrlLoader from "./VideoUrlLoader";
 
-import VideoEditAnimator from "./modules/VideoEditAnimator";
-import VideoEditDownloader from "./modules/VideoEditDownloader";
-import VideoEditTimeController from "./modules/VideoEditTimeController";
-
-const videoEditModulePackage: VideoEditModulePackage = {
-  animator: new VideoEditAnimator(),
-  downloader: new VideoEditDownloader(),
-  timeController: new VideoEditTimeController(),
-};
-
 export const videoRecord = new VideoRecord();
-export const videoEditEngine = new VideoEditEngine(videoEditModulePackage);
+export const videoEditEngine = new VideoEditEngine();
 export const videoUrlLoader = new VideoUrlLoader();
